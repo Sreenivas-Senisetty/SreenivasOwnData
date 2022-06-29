007ae3211ee70a7310150f81790090dcc25e55c6
Sreenivas-Senisetty <sreenivas.senisetty@gmail.com>
2022-06-29 18:58:58 +0530

    adding new files

---
+++ GitCommands.java
@@ -0,0 +1,9 @@
+git add *
+git commit -m ""
+git push az master
+git --global --list
+git remote -v
+git config --global user.name "username"
+git config --global user.mail "mailid"
+git init
+git status
